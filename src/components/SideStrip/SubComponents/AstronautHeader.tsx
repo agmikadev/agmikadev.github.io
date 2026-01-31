b9c0693582ad5e322a47e57928e14d04f27d66ce
angelomikaa <agelomikael2@gmail.com>
2026-01-31 02:59:42 -0300

    case sensitive fix for profile img

--- src/components/SideStrip/SubComponents/AstronautHeader.tsx
+++ src/components/SideStrip/SubComponents/AstronautHeader.tsx
@@ -11,7 +11,7 @@ export const AstronautHeader = () => (
       {/* A Foto ou Iniciais */}
       <div className="avatar-placeholder">
         <img 
-          src="/public/Angelo.jpg"
+          src="/public/angelo.jpg"
           alt="Mikael Angelo" 
           className="avatar-image"
         />
